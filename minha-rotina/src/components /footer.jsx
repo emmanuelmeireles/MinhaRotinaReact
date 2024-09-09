779c97aeaaf4cbc9e16c5e3d9c2b2ba642c0929d
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2024-09-09 19:18:48 +0000

    bruno me ajudaaaaa

---
+++ minha-rotina/src/components /footer.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Minha Rotina. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ minha-rotina/src/components /header.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Minha Rotina Diária</h1>
+    </header>
+  );
+};
+
+export default Header;
